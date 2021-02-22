0ce6346196b0f5f5ecfe03793be790f4c10d6c27
Sarehon Taylor <sarehontaylor@icloud.com>
2021-02-22 15:03:24 -0500

    Got the CSS working along with the Play button to cycle though test questions

--- game.js
+++ game.js
@@ -1,57 +1,88 @@
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName('choice-text'));
+const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
-let question Counter = 0;
-let availableQuestions =[];
+let questionCounter = 0;
+let availableQuesions = [];
 
-let questions = [...    
+let questions = [
+  {
+    question: "TEST QUESTION 1??",
+    choice1: "1",
+    choice2: "2",
+    choice3: "3",
+    choice4: "4",
+    answer: 1
+  },
+  {
+    question:
+      "TEST?",
+    choice1: "1",
+    choice2: "2",
+    choice3: "3",
+    choice4: "4",
+    answer: 3
+  },
+  {
+    question: " TEST AGAIN",
+    choice1: "1",
+    choice2: "2",
+    choice3: "3",
+    choice4: "4",
+    answer: 4
+  }
+];
 
-    question: "Inside which "
-
-
-
- //CONSTANTS//
+//CONSTANTS
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
+};
 
-startGame = () {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    console.log(availableQuestions);
-    getNewQuestion();
-    };
+getNewQuestion = () => {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    //go to the end page
+    return window.location.assign("/end.html");
+  }
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
 
-getNewQuestion = () {
-    questionCounter++;
-    const questionIndex =Math.floor(math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
-
-choices.forEach(choice --> {
+  choices.forEach(choice => {
     const number = choice.dataset["number"];
     choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
 });
 
-}
-
 startGame();
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    startGame();
