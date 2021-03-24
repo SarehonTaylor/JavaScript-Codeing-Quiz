d4c78ae2cdf1d0508b6326f3aaefe8fc59a1c7a4
Sarehon Taylor <sarehontaylor@icloud.com>
2021-03-24 19:08:50 -0400

    added timer logic to game.js

--- game.js
+++ game.js
@@ -1,11 +1,16 @@
+let currentQuestionindex = 0;
+let time = questions.length * 15;
+let timerid;
+
 const question = document.getElementById('question');
 const choicesEl = document.getElementsById('choice');
-const timerEl = document.getElementsById('time')
-const submitbutton = document.getElementsById('submit')
-const startbutton = document.getElementsById('start')
-const intialsEl = document.getElementsById('intials')
-const feedbackEl = document.getElementsById('feedback')
+const timerEl = document.getElementsById('time');
+const submitbutton = document.getElementsById('submit');
+const startbutton = document.getElementsById('start');
+const intialsEl = document.getElementsById('intials');
+const feedbackEl = document.getElementsById('feedback');
 const progressText = document.getElementById('progressText');
+
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
 let currentQuestion = {};
