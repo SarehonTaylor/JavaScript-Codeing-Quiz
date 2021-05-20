b11fd912bddc6063d996674f2443efc535cf3a03
Sarehon Taylor <74032335+SarehonTaylor@users.noreply.github.com>
2021-05-20 18:18:47 -0400

    Update end.js
    changed window location to fix broken link

--- end.js
+++ end.js
@@ -26,5 +26,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('highscore.html');
+    window.location.href='highscore.html';
 };
